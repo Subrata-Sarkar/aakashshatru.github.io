cc6ab306f9ee552b9dafd33ce3533e9eff46f4ee
Subrata-Sarkar <85308527+Subrata-Sarkar@users.noreply.github.com>
2024-01-22 20:18:24 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1189,7 +1189,7 @@ $.init = function() {
         ctx: $.ctxmg,
         x: $.cw / 2,
         y: $.ch - 172,
-        text: 'CREATED BY DEVANKIT SARKAR',
+        text: 'CREATED BY DHIRAJ SAHOO',
         hspacing: 1,
         vspacing: 1,
         halign: 'center',
